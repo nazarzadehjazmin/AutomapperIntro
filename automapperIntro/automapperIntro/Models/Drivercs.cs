869345bee86a0c9c53f94271c75b88ded0af70d0
nazarzadehjazmin <nazarzadehjazmin@gmail.com>
2023-07-02 20:55:40 -0300

    add models

---
+++ automapperIntro/automapperIntro/Models/Drivercs.cs
@@ -0,0 +1,14 @@
+﻿namespace automapperIntro.Models
+{
+    public class Drivercs
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DriverNumber { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int Status { get; set; }
+        public int WorldChampionships { get; set; }
+    }
+}
